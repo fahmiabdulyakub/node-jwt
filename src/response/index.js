7c3e7bde2f73796e8a6ae161db1462f43e8632a9
febrimaulana <febriyunus@gmail.com>
2020-09-05 17:06:15 +0700

    fix response data

--- src/response/index.js
+++ src/response/index.js
@@ -1,5 +1,6 @@
 exports.success200 = (data, time, req, res) => {
     const ipSource = (req.headers['x-forwarded-for'] || req.connection.remoteAddress || req.socket.remoteAddress || req.connection.socket.remoteAddress).split(",")[0];
+    delete req.body.password;
 
     const jsonData = {
         statusCode: 200,
@@ -16,6 +17,7 @@ exports.success200 = (data, time, req, res) => {
 
 exports.error401 = (message, time, req, res) => {
     const ipSource = (req.headers['x-forwarded-for'] || req.connection.remoteAddress || req.socket.remoteAddress || req.connection.socket.remoteAddress).split(",")[0];
+    delete req.body.password;
 
     const jsonData = {
         statusCode: 401,
@@ -26,11 +28,12 @@ exports.error401 = (message, time, req, res) => {
     res.status(401).json(jsonData);
     res.end();
     console.log(`REQUEST: IP${ipSource}`, req.method, req.url, JSON.stringify(req.method === 'GET' ? req.query : req.body));
-    console.log("SUCCESS RESPONE:", JSON.stringify(jsonData));
+    console.log("ERROR RESPONE:", JSON.stringify(jsonData));
 }
 
 exports.error403 = (message, time, req, res) => {
     const ipSource = (req.headers['x-forwarded-for'] || req.connection.remoteAddress || req.socket.remoteAddress || req.connection.socket.remoteAddress).split(",")[0];
+    delete req.body.password;
 
     const jsonData = {
         statusCode: 403,
@@ -41,11 +44,12 @@ exports.error403 = (message, time, req, res) => {
     res.status(403).json(jsonData);
     res.end();
     console.log(`REQUEST: IP${ipSource}`, req.method, req.url, JSON.stringify(req.method === 'GET' ? req.query : req.body));
-    console.log("SUCCESS RESPONE:", JSON.stringify(jsonData));
+    console.log("ERROR RESPONE:", JSON.stringify(jsonData));
 }
 
 exports.error500 = (error, time, req, res) => {
     const ipSource = (req.headers['x-forwarded-for'] || req.connection.remoteAddress || req.socket.remoteAddress || req.connection.socket.remoteAddress).split(",")[0];
+    delete req.body.password;
 
     const jsonData = {
         statusCode: 500,
@@ -57,5 +61,5 @@ exports.error500 = (error, time, req, res) => {
     res.status(500).json(jsonData);
     res.end();
     console.log(`REQUEST: IP${ipSource}`, req.method, req.url, JSON.stringify(req.method === 'GET' ? req.query : req.body));
-    console.log("SUCCESS RESPONE:", JSON.stringify(jsonData));
+    console.log("ERROR RESPONE:", JSON.stringify(jsonData));
 }
